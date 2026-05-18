490e5d21cc65d777055357a043428a880640c47e
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-18 15:50:36 +0530

    Update hashmap.py

--- Data Structures & Algorithms/is-anagram/hashmap.py
+++ Data Structures & Algorithms/is-anagram/hashmap.py
@@ -1,3 +1,14 @@
+#An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+#algorithm:
+#if 2 strings have different length, return False immediately
+#create 2 hashsets for both strings
+#iterate through both strings at same time
+#increment count of s[i]
+# increase the count of t[i]
+#compare the both maps,
+#if equal, return True
+#else return False
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -9,3 +20,5 @@ class Solution:
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
         return countS == countT
+#T(n)=O(n+m)
+#Space O(1)
