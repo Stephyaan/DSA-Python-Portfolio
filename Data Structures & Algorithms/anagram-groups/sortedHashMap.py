58801902739b368fe276a1ef74a5dee2a5974c82
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-18 19:10:26 +0530

    Update sortedHashMap.py

--- Data Structures & Algorithms/anagram-groups/sortedHashMap.py
+++ Data Structures & Algorithms/anagram-groups/sortedHashMap.py
@@ -10,8 +10,8 @@ class Solution:
         result=defaultdict(list)  #. It automatically creates an empty list ([]) as the value whenever you access a key that doesn't exist yet. 
         for s in strs:
             sortedS=''.join(sorted(s))  #sorted(s): The built-in sorted() function takes the string s and breaks it into a list of individual characters, 
-                                        #then sorts them (e.g., "cba" becomes ['a', 'b', 'c']).
-#                                      ''.join(...): The join() method takes that list of characters and stitches them back together into a single string, 
+                                         #then sorts them (e.g., "cba" becomes ['a', 'b', 'c']).
+                                         #''.join(...): The join() method takes that list of characters and stitches them back together into a single string, 
                                         #using an empty string '' as the separator.
             result[sortedS].append(s)
         return list(result.values())
