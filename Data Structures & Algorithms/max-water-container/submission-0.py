e3e13be73bfa043ac629562de004c9c5ac54ae46
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-20 21:15:55 +0530

    Update and rename submission-0.py to byteforce.py

--- Data Structures & Algorithms/max-water-container/submission-0.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxArea(self, heights: List[int]) -> int:
-        result=0
-        for l in range(len(heights)):
-            for r in range(l+1,len(heights)):
-                area=(r-l)*min(heights[l],heights[r])
-                result=max(result,area)
-        return result
