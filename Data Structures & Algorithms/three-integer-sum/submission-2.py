78bda8587dfcd9a9060d1d83c0fd731f921d4711
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-20 18:23:19 +0530

    Update submission-2.py

--- Data Structures & Algorithms/three-integer-sum/submission-2.py
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -1,27 +1,31 @@
+Algorithm
+Sort the array to make handling duplicates easier.
+Create an empty set res to store unique triplets.
+Use three nested loops:
+For each i,
+For each j > i,
+For each k > j,
+Check if nums[i] + nums[j] + nums[k] == 0.
+If true, add the sorted triplet to the set.
+Convert the set of tuples back into a list of lists.
+Return the result.
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
 
-        for i, a in enumerate(nums):
-            if a > 0:
-                break
+        output = []
 
-            if i > 0 and a == nums[i - 1]:
-                continue
+        for i in range(len(nums)):
 
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
-                else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
+            for j in range(i+1, len(nums)):
 
-        return res
+                for k in range(j+1, len(nums)):
+
+                    if (nums[i] + nums[j] + nums[k]) == 0:
+
+                        temp = sorted([nums[i], nums[j], nums[k]])
+
+                        if temp not in output:
+                            output.append(temp)
+
+        return output
