4c0a84b74a7e7058774016fb6130b31bd62a0029
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-24 20:29:08 +0530

    Update SlidingWindow(optimal).py

--- Data Structures & Algorithms/longest-repeating-substring-with-replacement/SlidingWindow(optimal).py
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/SlidingWindow(optimal).py
@@ -21,10 +21,10 @@ class Solution:
             count[s[r]]=1+count.get(s[r],0)  #0 if s[r] not in count{}
             maxf=max(maxf,count[s[r]])
 
-            while ((r-l+1)-maxf) >k:    #valid if (r-l+1)-maxf <= k
-                count[s[l]]-=1
+            while ((r-l+1)-maxf) >k:    #valid only if (r-l+1)-maxf <= k
+                count[s[l]]-=1          #invalid
                 l+=1
-            res=max(res,r-l+1)
+            res=max(res,r-l+1)  #valid
         return res
 
 #time:O(n*m)
