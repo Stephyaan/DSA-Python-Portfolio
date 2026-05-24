fe36437b48fa89b5a50d6952f233c74a81b02db3
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-24 16:45:13 +0530

    Update hashSet.py

--- Data Structures & Algorithms/longest-consecutive-sequence/hashSet.py
+++ Data Structures & Algorithms/longest-consecutive-sequence/hashSet.py
@@ -1,3 +1,7 @@
+#qn: Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. 
+The elements do not have to be consecutive in the original array.
+You must write an algorithm that runs in O(n) time.
 #Algorithm
 #Convert the list into a set numSet for O(1) lookups.
 #Initialize longest to track the length of the longest consecutive sequence.
