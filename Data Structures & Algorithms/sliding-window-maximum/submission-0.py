6197d7f7c39e7bc8b6beecfda5056c2335c38025
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-25 01:11:38 +0530

    Update submission-0.py

--- Data Structures & Algorithms/sliding-window-maximum/submission-0.py
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.py
@@ -1,3 +1,13 @@
+#Qn: You are given an array of integers nums and an integer k. There is a sliding window of size k that starts at the left edge of the array. 
+The window slides one position to the right until it reaches the right edge of the array.Return a list that contains the maximum element in the window at each step.
+#Algorithm:
+#Create an empty list, maxL, output to store the maximum of each window.
+#For each starting index i from 0 to len(nums):
+#check if length of substring between i and i+k < k
+    #if yes,break
+    #else, append to maxL the maximum in the substring 
+#Return output list maxL after checking all windows.
+    
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         maxL=[]
@@ -6,3 +16,5 @@ class Solution:
                 break
             maxL.append(max(nums[i:(i+k)]))
         return maxL
+#timeO(n*k)
+#space O(n)
