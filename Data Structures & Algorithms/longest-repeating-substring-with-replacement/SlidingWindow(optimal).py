f0c0c41a6909a3a01a3e2205d6d3c162ba7419bb
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-24 20:24:19 +0530

    Update and rename submission-0.py to SlidingWindow(optimal).py

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/SlidingWindow(optimal).py
@@ -0,0 +1,32 @@
+#Qn: You are given a string s consisting of only uppercase english characters and an integer k. 
+You can choose up to k characters of the string and replace them with any other uppercase English character.
+After performing at most k replacements, return the length of the longest substring which contains only one distinct character.
+#algorithm:
+#Create a frequency map count and initialize left pointer,l = 0, maxf = 0, and size of longest substring,res = 0.
+#Move the right pointer r across the string through loop:
+    #Update the frequency of s[r].
+    #Update maxf with the highest frequency seen so far.
+#If the window size, (r-l-1), is invalid (window size - maxf > k):
+    #Shrink the window from the left and adjust counts.
+#Update the result with the valid window size.
+#Return res at the end.
+    
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count={}
+        l=0
+        res=0
+        maxf=0
+        for r in range(len(s)):
+            count[s[r]]=1+count.get(s[r],0)
+            maxf=max(maxf,count[s[r]])
+
+            while ((r-l+1)-maxf) >k:
+                count[s[l]]-=1
+                l+=1
+            res=max(res,r-l+1)
+        return res
+
+#time:O(n*m)
+#space:O(m)
+        
