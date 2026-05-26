eb17f22a4db6c2fd73f4aae53d88d570cb3d5c2a
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-27 02:13:16 +0530

    Update and rename submission-0.py to bruteforce.py

--- Data Structures & Algorithms/remove-node-from-end-of-linked-list/bruteforce.py
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/bruteforce.py
@@ -1,3 +1,11 @@
+#Qn: Given the head of a linked list and an integer n, remove the nth node from the end of the list and return its head.
+#algrithm:
+#Traverse the linked list and push every node into an array.
+#Compute the index of the node to remove: len(nodes) - n.
+#If this index is 0, it means the head must be removed → return head.next.
+#Otherwise, connect nodes[removeIndex - 1].next to nodes[removeIndex].next.
+#Return the updated head.
+sol:
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -12,7 +20,7 @@ class Solution:
             dummy.append(curr)
             curr=curr.next
         removeIndex=len(dummy)-n
-        if removeIndex==0:
+        if removeIndex==0:        #head removed 
             return head.next
         dummy[removeIndex-1].next=dummy[removeIndex].next  #next of prev node of removeIndex node is node next to removeIndex node.
         return head                                        #so removeIndex node not there.prev connected to its next
