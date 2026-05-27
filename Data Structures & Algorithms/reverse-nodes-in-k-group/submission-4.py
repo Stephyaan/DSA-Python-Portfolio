95469353e5509ac73fd54da6d314024779d0d8e2
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-28 03:31:11 +0530

    Update submission-4.py

--- Data Structures & Algorithms/reverse-nodes-in-k-group/submission-4.py
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-4.py
@@ -1,3 +1,22 @@
+'''You are given the head of a singly linked list head and a positive integer k.You must reverse the first k nodes in the linked list, 
+and then reverse the next k nodes, and so on. If there are fewer than k nodes left, leave the nodes as they are.
+Return the modified list after reversing the nodes in each group of k.
+You are only allowed to modify the nodes' next pointers, not the values of the nodes.'''
+'''Algorithm:
+Create an empty array.
+Traverse the linked list from head node.
+Store each node value into the array.
+Traverse the array in steps of size k.
+For each group:
+Check whether k elements exist.
+If yes, reverse that group.
+If less than k elements remain, leave them unchanged.
+Create a dummy node for the new linked list.
+Traverse the modified array.
+Create new nodes using array values and attach them to the new linked list.
+Return the linked list starting after the dummy node.
+'''
+sol:
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
