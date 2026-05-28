03625b018af9d9f3700bb5e0eb299af5264357fe
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-29 01:26:34 +0530

    Update and rename submission-0.py to bruteforce.py

--- Data Structures & Algorithms/daily-temperatures/bruteforce.py
+++ Data Structures & Algorithms/daily-temperatures/bruteforce.py
@@ -1,3 +1,15 @@
+'''You are given an array of integers temperatures where temperatures[i] represents the daily temperatures on the ith day.
+Return an array result where result[i] is the number of days after the ith day before a warmer temperature appears on a future day. 
+If there is no day in the future where a warmer temperature will appear for the ith day, set result[i] to 0 instead.'''
+'''Algorithm
+Let res store the number of days until a warmer temperature.
+For each index i:
+Start checking from the next day j = i + 1 and count how many steps it takes to find a warmer day.
+If a warmer day is found, store the count.
+Otherwise, store 0.
+Return the result array.
+'''
+sol:
 class Solution:
     def dailyTemperatures(self, temps: List[int]) -> List[int]:
         result=[]
@@ -22,3 +34,6 @@ class Solution:
             result.append(count)  #for i'th temp,place count at result i'th place
 
         return result
+
+#time: O(n^2)
+#Space:O(1);O(n) for output
