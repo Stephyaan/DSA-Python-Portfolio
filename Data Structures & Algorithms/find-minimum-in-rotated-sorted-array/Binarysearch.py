69cf6913d0102a9ea8b0e900b733fbac3f0f9e51
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-05-31 00:20:00 +0530

    Update Binarysearch.py

--- Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/Binarysearch.py
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/Binarysearch.py
@@ -1,32 +1,22 @@
 #Time: O(logn) ; Space : O(1)
 '''n asked specifically time complexity of O(logn) ; Binary search is optimal here '''
 '''Algorithm
-Initialize left = 0, right = n - 1, and store the first element as the current answer.
-While left <= right:
-If the current window is already sorted, update the answer with nums[left] and stop.
+Set left = 0 and right = n - 1.
+While left < right:
 Compute mid.
-Update the answer with nums[mid].
-If the left half is sorted:
-Move search to the right half.
-Otherwise:
-Search in the left half.
-Return the smallest value found.
+If nums[mid] is less than nums[right], move right to mid (minimum is on the left).
+Otherwise, move left to mid + 1 (minimum is on the right).
+When the loop ends, left points to the smallest element.
+Return nums[left].
 '''
 Sol:
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        res = nums[0]
         l, r = 0, len(nums) - 1
-
-        while l <= r:
-            if nums[l] < nums[r]:
-                res = min(res, nums[l])
-                break
-
-            m = (l + r) // 2
-            res = min(res, nums[m])
-            if nums[m] >= nums[l]:
-                l = m + 1
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] < nums[r]:
+                r = m
             else:
-                r = m - 1
-        return res
+                l = m + 1
+        return nums[l]
