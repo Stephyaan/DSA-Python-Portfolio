54513b521198b8c7aee10675ae56375719d78d5a
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-06-04 21:37:34 +0530

    Update submission-1.py

--- Data Structures & Algorithms/balanced-binary-tree/submission-1.py
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -1,3 +1,15 @@
+'''Given a binary tree, return true if it is height-balanced and false otherwise.
+A height-balanced binary tree is defined as a binary tree in which the left and right subtrees of every node differ in height by no more than 1.'''
+'''Algorithm:
+if root is null return true(balanced=0)
+compute leftdepth and rightdepth for left subtree Nand right subtree
+if abs(leftdepth-rightdepth) > 1 , return false
+check if 
+    left subtree balanced
+    right subtree balanced
+return true if all checks passed as balanced
+'''
+sol:
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -17,8 +29,8 @@ class Solution:
             return False
         else :
             return self.isBalanced(root.left) and self.isBalanced(root.right)
-        
-        
+            #checking repeatedly for each subtree if balanced/not
+    
 
     def maxDepth(self,root:Optional[TreeNode])->int:
         if not root:
@@ -27,3 +39,6 @@ class Solution:
         rightDepth=self.maxDepth(root.right)
 
         return 1+max(leftDepth,rightDepth)
+
+#time:O(n^2)
+#space:O(n)
