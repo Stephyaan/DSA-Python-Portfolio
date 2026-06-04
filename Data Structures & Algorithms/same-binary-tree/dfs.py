66ab567604b8cc876f3c6ae78a142cdc65bfca5f
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-06-05 02:26:50 +0530

    Update and rename submission-0.py to dfs.py

--- Data Structures & Algorithms/same-binary-tree/dfs.py
+++ Data Structures & Algorithms/same-binary-tree/dfs.py
@@ -1,3 +1,15 @@
+'''Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.'''
+'''Algorithm
+check if both are null, return true
+if any one null, return false
+if both null, and values of p and q are equal
+recursively compare 
+    with p.left and q.left
+    with p.right and q.right
+return true only if both subtrees comparisons are equal
+'''
+sol:
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -15,5 +27,6 @@ class Solution:
             return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
         else: #if fails
             return False
-
+#time:O(n^2)
+#space:O(n)
         
