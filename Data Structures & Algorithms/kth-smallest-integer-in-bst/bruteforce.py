4082a18439d0590bd9dfb79e53ba0af270902f36
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-06-08 19:21:11 +0530

    Update and rename submission-2.py to bruteforce.py

--- Data Structures & Algorithms/kth-smallest-integer-in-bst/bruteforce.py
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/bruteforce.py
@@ -1,3 +1,17 @@
+'''Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) in the tree.
+A binary search tree satisfies the following constraints:
+The left subtree of every node contains only nodes with keys less than the node's key.
+The right subtree of every node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees are also binary search trees.'''
+'''Algorithm:
+initialise an empty array to store nodes
+traverse by dfs:
+   append the node value to the array
+   traverse through the left subtree and insert node values to array as elements
+   traverse the right subtree and append node values to arr
+sort the array
+return the kth smallest element
+'''
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -18,6 +32,9 @@ class Solution:
             arr.append(node.val)#nodes appended
             dfs(node.left) #start with left subtree;smallest nodes
             dfs(node.right) #then start with right subtree;greater nodes
+
+    #time:O(nlogn) sorting-logn and traversing
+    #space:O(n)
         
         #start traversing from root
         dfs(root)
