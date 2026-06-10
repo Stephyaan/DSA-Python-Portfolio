d4eddc379aa8bd8c7f75fd9b5a215326cbf8f4c5
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-06-10 15:11:44 +0530

    Update and rename dfs.py to dfsString.py

--- Data Structures & Algorithms/serialize-and-deserialize-binary-tree/dfsString.py
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/dfsString.py
@@ -1,3 +1,9 @@
+'''Implement an algorithm to serialize and deserialize a binary tree.
+Serialization is the process of converting an in-memory structure into a sequence of bits so that it can be stored or sent across 
+a network to be reconstructed later in another computer environment.You just need to ensure that a binary tree can be 
+serialized to a string and this string can be deserialized to the original tree structure. There is no additional restriction on 
+how your serialization/deserialization algorithm should work.Note: The input/output format in the examples is the same as how NeetCode 
+serializes a binary tree. You do not necessarily need to follow this format.'''
 '''Algorithm:
 serialize():
 create an empty string ser to store node values
