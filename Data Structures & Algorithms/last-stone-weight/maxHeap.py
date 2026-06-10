a53d4a0489e328ed832f0c1cbeff04ce6328884d
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-06-11 03:03:01 +0530

    Update and rename submission-1.py to maxHeap.py

--- Data Structures & Algorithms/last-stone-weight/maxHeap.py
+++ Data Structures & Algorithms/last-stone-weight/maxHeap.py
@@ -1,3 +1,14 @@
+'''Algorithm
+Convert every stone weight x into -x and build a min-heap.
+While the heap contains more than one stone:
+Pop two values a and b (these represent the two heaviest stones).
+If a != b, compute the remaining stone weight:
+diff = a - b (still negative)
+Push diff back into the heap.
+After the loop:
+If the heap is empty, return 0.
+Otherwise return the absolute value of the remaining stone.
+'''
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         #multply every val with -1 as maxHeap not in Python
@@ -13,3 +24,5 @@ class Solution:
 
         stones.append(0)  #if len stones not > 1; no stones left, need to return 0
         return abs(stones[0])  #return final stone weight left
+#time:O(n log n)
+#space:O(n) or O(1)
