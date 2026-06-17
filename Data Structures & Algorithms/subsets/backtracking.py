0309071e53269d94ac259d5c3227683cb3d4f235
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-06-17 20:51:00 +0530

    Update and rename submission-0.py to backtracking.py

--- Data Structures & Algorithms/subsets/backtracking.py
+++ Data Structures & Algorithms/subsets/backtracking.py
@@ -1,3 +1,23 @@
+'''Given an array nums of unique integers, return all possible subsets of nums.
+The solution set must not contain duplicate subsets. You may return the solution in any order.Example 1:
+Input: nums = [1,2,3] ; Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]'''
+'''algorithm:
+create res array to store subsets
+initialize a subset array
+define a recurring function dfs(i):
+    if i equals len(nums):
+        add the copy of subset to res array
+        return
+    if choice1-to add element to subset:
+        append to subset array
+        recurse to next index (call dfs(i+1) with next index)
+    if choice2-not to add element to subset:
+        (pop the recently added from subset)
+        recurse to next index (call dfs(i+1) with next index)
+
+start recursion from start index dfs(0)
+return the res array
+'''
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         #idea and concept of decision tree
@@ -23,5 +43,6 @@ class Solution:
         
         dfs(0) #start from start 
         return res
-
-            
+#time:O(n*2^n)    2^n subsets 
+#space: O(2^n) for output array
+#O(n) extra space
