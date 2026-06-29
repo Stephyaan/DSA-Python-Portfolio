6172c9a88a27d9d198603f6fbdaed88fbeeb5c43
Stephy Ann Biju <stephyannbiju@gmail.com>
2026-06-29 21:42:34 +0530

    Update submission-0.py

--- Data Structures & Algorithms/search-for-word-ii/submission-0.py
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.py
@@ -1,6 +1,22 @@
 '''Given a 2-D grid of characters board and a list of strings words, return all words that are present in the grid.
 For a word to be present it must be possible to form the word with a path in the board with horizontally 
 or vertically neighboring cells. The same cell may not be used more than once in a word.'''
+'''Algorithm
+Build a Trie from all words.
+Each Trie node stores children (next letters) and isWord (true if a word ends here).
+Initialize:
+res as a set (to avoid duplicates).
+visit as a set for the current DFS path.
+Define DFS dfs(r, c, node, wordSoFar):
+If (r,c) is out of bounds, already visited, or board[r][c] is not in node.children, stop.
+Mark (r,c) visited.
+Move Trie pointer: node = node.children[board[r][c]]
+Append current char to wordSoFar.
+If node.isWord == true, add wordSoFar to res.
+Recurse to 4 neighbors (up/down/left/right) using the updated node and wordSoFar.
+Backtrack: remove (r,c) from visit.
+Run DFS starting from every cell (r, c) with the Trie root.
+Return all collected words from res.'''
 
 class TrieNode:
     def __init__(self):
@@ -45,7 +61,8 @@ class Solution:
         for r in range(Rows):
             for c in range(Cols):
                 dfs(r,c,root,"")
-
+#time : O(m*n*4*3^t-1 +s)
+        #space:O(s)
 
         return list(res)
 
